29497069be8a3f58a09285c5b526d31c945f4524
mminhou <exit19093@gmail.com>
2022-04-14 10:06:40 +0900

    Feat: web sdk sample 페이지 1차 완성

---
+++ src/renderer/PenBasedRenderer.tsx
@@ -0,0 +1,64 @@
+import { makeStyles } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import PenHelper from '../utils/PenHelper';
+import { fabric } from 'fabric';
+
+const useStyle = makeStyles(() => ({
+  mainBackground: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+}));
+
+const PenBasedRenderer = () => {
+  const classes = useStyle();
+  const [canvasFb, setCanvasFb] = useState<fabric.Canvas>(null);
+  const [ctx, setCtx] = useState(null);
+
+  useEffect(() => {
+    setCanvasFb(initCanvas());
+  }, []);
+
+  useEffect(() => {
+    PenHelper.dotCallback = (mac, dot) => {
+      setCtx(canvasFb.getContext());
+      strokeProcess(dot);
+    }
+  });
+
+  const initCanvas = () => { 
+    return new fabric.Canvas('sampleCanvas', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }); 
+  }
+
+  const strokeProcess = (dot) => {
+    if (dot.dotType === 0) {
+      ctx.beginPath();
+    } else if (dot.dotType === 1) {
+      if (dot.x > 1000 || dot.y > 1000) {
+        return
+      }
+      ctx.lineWidth = 2;
+      ctx.lineTo(dot.x * 10, dot.y * 10);
+      ctx.stroke();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.moveTo(dot.x * 10, dot.y * 10);
+    } else {
+      ctx.closePath();
+    }
+  }
+
+  return (
+    <div className={classes.mainBackground}>
+      <canvas id="sampleCanvas" width={window.innerWidth} height={window.innerHeight}></canvas>
+    </div>
+  );
+};
+
+export default PenBasedRenderer;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              outputPath: './assets/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    fallback: {
+      "zlib": false
+    } 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      inject: true,
+      filename: path.resolve(__dirname, './dist/index.html'),
+      favicon: './public/favicon.ico',
+    }),
+  ],
+  mode: 'development',
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+}
